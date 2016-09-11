9ea8e08731bdd3ac19b91a186555371fe75e91e0
Dennis Herrmann <dhn@4bit.ws>
2016-09-11 23:02:22 +0200

    Add license text in enpass2pass.py and bump id

--- enpass2pass.py
+++ enpass2pass.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
-# $Id: enpass2pass.py,v 1.0 2016/09/04 23:45:04 dhn Exp $
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This script based on the Enpassant
+# script written by Steffen Zerbe.
+#
+# $Id: enpass2pass.py,v 1.1 2016/09/11 23:02:12 dhn Exp $
 
 from subprocess import Popen, PIPE
 from pysqlcipher import dbapi2 as sqlite
